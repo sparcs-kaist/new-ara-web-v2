6debdf39876486c9a4e1e9af0da4e49373b37a01
sum37 <82599165+sum37@users.noreply.github.com>
2025-03-17 15:25:34 +0900

    searchBar

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,32 +1,43 @@
-import React from "react";
-import Image from "next/image"
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 interface TextInputProps {
-  label?: string;
-  placeholder?: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onReset: () => void; // 버튼 클릭 시 실행될 함수 추가
 }
 
-const SearchBar: React.FC<TextInputProps> = ({ label, value, onChange, onReset }) => {
+const SearchBar: React.FC<TextInputProps> = ({ value, onChange}) => {
+  const router = useRouter();
+
+  // 검색 실행 함수
+  const handleSearch = () => {
+    if (value.trim() !== "") {
+      router.push(`/board?query=${encodeURIComponent(value)}`);
+    }
+  };
+
+  // Enter 키 입력 처리
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
-    <div className="flex items-center space-x-2 w-[550px] h-[56px] bg-red-50">
-      <div className="flex flex-col flex-grow">
-        {label && <label className="text-sm font-medium text-gray-700">{label}</label>}
-        <input
-          type="text"
-          className="border rounded-md p-2 w-full focus:none"
-          placeholder="검색어를 입력하세요."
-          value={value}
-          onChange={onChange}
-        />
-      </div>
-      <button
-        className="px-4 py-2 text-white rounded-md h-[28px] w-[28px]"
-        onClick={onReset} // 버튼 클릭 시 실행
-      >
-        <Image src="/search.png" width={28} height={28} alt="검색"/>
+    <div className="flex h-[56px] border-[2px] border-ara_red rounded-[16px] px-4 py-2 space-x-2 w-[90%] max-w-[550px]">
+      <input
+        type="search"
+        className="w-full focus:outline-none focus:ring-0 placeholder-ara_red_bright"
+        placeholder="검색어를 입력하세요."
+        value={value}
+        onChange={onChange}
+        onKeyDown={handleKeyDown} 
+      />
+      <button onClick={handleSearch}>
+        <Image src="SearchBar_search.svg" width={28} height={28} alt="search" />
       </button>
     </div>
   );
