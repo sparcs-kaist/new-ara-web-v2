623f9e04252663bcf77e3b00005e05902d6e1496
sparcscasio <162997498+sparcscasio@users.noreply.github.com>
2025-06-10 00:22:41 +0900

    Revert "feat/profile"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,12 +9,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        ara_red: "#ED3A3A",
-        ara_red_bright: "#F9C7C7",
-        ara_red_most_bright: "#FDF0F0",
-        ara_blue: "#538DD1",
-        ara_gray: "#B5B5B5",
-        ara_gray_bright: "#B8B8B8"
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
