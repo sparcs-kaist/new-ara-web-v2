d46362e159cfef5c2f7dfbae87d5e2fb295a73b5
SeungBeom-Park <killerwhale@sparcs.org>
2025-06-19 00:25:35 +0900

    complete login page ui

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,19 +16,26 @@ export default function LoginPage() {
         <p className="text-center text-[#ed3a3a] font-semibold text-[21px] lg:text-[32px]">가장 정확한 정보를</p>
         <p className="text-center text-[#ed3a3a] font-semibold text-[21px] lg:text-[32px]">가장 신속하게</p>
       </div>
-      <div className="w-full flex-1 bg-white lg:h-full">
+      <div className="flex flex-col w-full flex-1 bg-white lg:h-full lg:justify-center">
         <div className="flex flex-col items-center justify-center px-4 py-12 text-center">
-          <div className="w-full">
-            <div className="h-1.5 w-6 bg-red-500 rounded-full mb-4" />
+          <div className="flex flex-col items-center w-full">
+            <div className="h-1.5 w-6 bg-red-500 mb-4" />
             <h1 className="text-[20px] lg:text-[28px] font-semibold text-gray-800 mb-2">
               뉴아라 서비스를 이용하시려면 로그인해주세요.
             </h1>
-            <p className="text-sm lg:text-base text-gray-600 mb-1">
-              교직원과 졸업생은 SPARCS SSO에 접속한 뒤
+            {/* lg 이상: 한 줄로 */}
+            <p className="hidden lg:block text-base text-gray-600 mb-8">
+            교직원과 졸업생은 SPARCS SSO에 접속한 뒤 {'<카이스트 통합인증으로 로그인/가입>'}을 선택해주세요.
             </p>
-            <p className="text-sm lg:text-base text-gray-600 mb-8">
-              {'<카이스트 통합인증으로 로그인/가입>'}을 선택해주세요.
+            {/* lg 미만: 두 줄로 */}
+            <>
+            <p className="text-sm text-gray-600 mb-1 lg:hidden">
+                교직원과 졸업생은 SPARCS SSO에 접속한 뒤
             </p>
+            <p className="text-sm text-gray-600 mb-8 lg:hidden">
+                {'<카이스트 통합인증으로 로그인/가입>'}을 선택해주세요.
+            </p>
+            </>
             <LoginPageButton
               redirectUrl="/login"
               fontSize={18}
@@ -39,16 +46,25 @@ export default function LoginPage() {
           </div>
         </div>
         <div className="flex flex-col items-center justify-center px-4 py-12 text-center">
-          <div className="w-full">
-            <div className="h-1.5 w-6 bg-red-500 rounded-full mb-4" />
+          <div className="flex flex-col items-center w-full">
+            <div className="h-1.5 w-6 bg-red-500 mb-4" />
             <h1 className="text-[20px] lg:text-[20px] font-semibold text-gray-800 mb-2">
               학생단체/입주업체 공용 계정을 만들고 싶으신가요?
             </h1>
-            <p className="text-sm lg:text-base text-gray-600 mb-8">
-              공용 이메일로 회원가입을 진행한 후, 아래 버튼을 클릭하여 신청서를 보내주세요.
+            {/* lg 이상: 한 줄 */}
+            <p className="hidden lg:block text-base text-gray-600 mb-8">
+            공용 이메일로 회원가입을 진행한 후, 아래 버튼을 클릭하여 신청서를 보내주세요.
             </p>
-
-            <div className="flex flex-col lg:flex-row gap-4 justify-center">
+            {/* lg 미만: 두 줄 */}
+            <>
+            <p className="text-sm text-gray-600 mb-1 lg:hidden">
+                공용 이메일로 회원가입을 진행한 후,
+            </p>
+            <p className="text-sm text-gray-600 mb-8 lg:hidden">
+                아래 버튼을 클릭하여 신청서를 보내주세요.
+            </p>
+            </>
+            <div className="flex flex-col w-[200px] lg:flex-row gap-4 justify-center lg:w-full">
               <LoginPageButton redirectUrl="https://bit.ly/sso-signup" fontSize={15}>
                 공용 이메일로 회원가입
               </LoginPageButton>
