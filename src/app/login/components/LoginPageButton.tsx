e4e75b5b65aa07063785cae2196dd41982aec3e8
SeungBeom-Park <killerwhale@sparcs.org>
2025-06-18 22:51:03 +0900

    add general button component in login page

---
+++ src/app/login/components/LoginPageButton.tsx
@@ -0,0 +1,38 @@
+//LoginPageButton.tsx
+//로그인 페이지에서 공통적으로 사용되는 버튼 컴포넌트
+
+"use client";
+
+import exp from "constants";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+interface LoginPageButtonProps {
+    icon ?: ReactNode | null;
+    fontSize ?: number; // px 단위
+    redirectUrl : string;
+    children : string; // Text inside the button
+}
+
+export default function LoginPageButton({
+  icon = null,
+  fontSize = 16,
+  redirectUrl,
+  children,
+}: LoginPageButtonProps) {
+  return (
+    <Link
+      href={redirectUrl}
+      className={`
+        inline-flex items-center gap-2 px-4 py-2
+        rounded-lg bg-white text-[#ed3a3a] border border-[#dbdbdb]
+        font-normal transition-shadow duration-300
+        hover:shadow-[0_2px_6px_0_#d1d5db]
+        text-[${fontSize}px]
+      `}
+    >
+      {icon && <span>{icon}</span>}
+      {children}
+    </Link>
+  );
+}
