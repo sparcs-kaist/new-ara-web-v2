9d558a135185910fb9edfb06b1246efc5129b26d
SeungBeom-Park <killerwhale@sparcs.org>
2025-06-19 16:06:17 +0900

    add QueryBuilder and basic ROP rule

---
+++ src/lib/utils/queryBuilder.ts
@@ -0,0 +1,6 @@
+export function queryBuilder(params: Record<string, any>): string {
+  return Object.entries(params)
+    .filter(([, value]) => value !== undefined && value !== null)
+    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+    .join('&');
+}
