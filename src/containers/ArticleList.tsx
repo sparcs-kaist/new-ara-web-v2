b8f202d8d0d2bc0208c45c88903976c603c0b522
SeungBeom-Park <killerwhale@sparcs.org>
2025-06-29 21:02:07 +0900

    apply pagenation to board all articles

--- src/containers/ArticleList.tsx
+++ src/containers/ArticleList.tsx
@@ -124,29 +124,36 @@ export function BoardArticleList({ boardId=7, pageSize = 10 }: BoardArticleListP
 //Board 페이지 - 전체 게시글
 export function BoardAllArticleList({ pageSize = 10 }: BoardArticleListProps) {
     const [posts, setPosts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+
     useEffect(() => {
         const fetchData = async () => {
-            const Response = await fetchArticles({pageSize: pageSize});
+            const Response = await fetchArticles({ pageSize: pageSize, page: currentPage });
             setPosts(Response.results);
+            setTotalPages(Response.num_pages || 1);
         }
         fetchData();
-    }, []);
+    }, [pageSize, currentPage]);
+
     return(
         <ArticleList
-            posts = {posts}
-            showBoard = {true}
-            showTimeAgo = {true}
-            showAttachment = {true}
-            showProfile = {true}
-            showWriter = {true}
+            posts={posts}
+            showBoard={true}
+            showTimeAgo={true}
+            showAttachment={true}
+            showProfile={true}
+            showWriter={true}
             titleFontSize='text-[16px]'
-            showTopic = {true}
-            showHit = {true}
-            showStatus = {true}
-            showAnswerStatus = {true}
-
-        >
-        </ArticleList>
+            showTopic={true}
+            showHit={true}
+            showStatus={true}
+            showAnswerStatus={true}
+            pagination={true}
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={setCurrentPage}
+        />
     )
 }
 
