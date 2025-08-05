12d05353a1f14e1f121ae3702015ecde6da97ac8
SeungBeom-Park <killerwhale@sparcs.org>
2025-08-05 18:39:33 +0900

    fix layout.tsx   fix excption logic

--- src/lib/api/http.ts
+++ src/lib/api/http.ts
@@ -38,10 +38,10 @@ http.interceptors.response.use(
       const status = error.response.status;
 
       if (typeof window !== 'undefined') {
-        if (status === 401) window.location.href = '/login';
-        else if (status === 404) window.location.href = '/404';
-        else if (status === 418) window.location.href = '/tos';
-        else if (status === 410) window.location.href = '/410';
+        if (status === 401 && window.location.pathname !== '/login') window.location.href = '/login';
+        else if (status === 404 && window.location.pathname !== '/404') window.location.href = '/404';
+        else if (status === 418 && window.location.pathname !== '/tos') window.location.href = '/tos';
+        else if (status === 410 && window.location.pathname !== '/410') window.location.href = '/410';
       }
 
       if (typeof error.response.data === 'object') {
