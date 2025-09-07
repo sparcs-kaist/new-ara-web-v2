27468e23e976afc391f4ff2c50061b7e2502d600
SeungBeom-Park <killerwhale@sparcs.org>
2025-09-08 03:46:39 +0900

    upgrade package

--- src/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-    return (
-        <div className="p-8 text-center">
-            <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
-            <p>The page you’re looking for doesn’t exist.</p>
-        </div>
-    )
-}
--- src/app/pages/404.tsx
+++
@@ -1,9 +0,0 @@
-// app/not-found.tsx
-export default function NotFound() {
-    return (
-        <div className="p-8 text-center">
-            <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
-            <p>The page you requested could not be found.</p>
-        </div>
-    );
-}
