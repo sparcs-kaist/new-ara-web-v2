c6013042a2be6b7c531586a0cf94459b3bb6aaab
SeungBeom-Park <killerwhale@sparcs.org>
2025-09-23 04:51:18 +0900

    fix notification redirection and ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,6 +66,22 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     );
   }
 
+  // 채팅 페이지 : Footer를 사용하지 않음
+
+  if (pathname.startsWith("/chat")) {
+    return (
+      <html lang="ko">
+        <head>
+          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+        </head>
+        <body className="h-screen">
+          <NavBar />
+          <main>{children}</main>
+        </body>
+      </html>
+    );
+  }
+
   // 일반 페이지 : NavBar와 Footer포함해서 렌더링
   return (
     <html lang="ko">
