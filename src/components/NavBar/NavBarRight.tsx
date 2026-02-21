df3f29ce45d19367ff2eafbccb1fa061163b57ec
SeungBeom-Park <killerwhale@sparcs.org>
2026-02-21 23:47:53 +0900

    add post write button on navbar and fix post write button policy

--- src/components/NavBar/NavBarRight.tsx
+++ src/components/NavBar/NavBarRight.tsx
@@ -1,14 +1,16 @@
 "use client";
 import NavBarProfile from "./NavBarProfile";
 import NotificationButton from "./NotificationButton";
+import PostWriteButton from "./PostWriteButton";
 
-export default function NavBarRight () {
-    return (
-      <div
-        className="flex items-center space-x-[10px] flex-shrink-0"
-        style={{marginRight: "clamp(10px, 5vw, 150px)"}}>
-        <NotificationButton />
-        <NavBarProfile />
-      </div>
-    )
+export default function NavBarRight() {
+  return (
+    <div
+      className="flex items-center space-x-[10px] flex-shrink-0"
+      style={{ marginRight: "clamp(10px, 5vw, 150px)" }}>
+      <PostWriteButton />
+      <NotificationButton />
+      <NavBarProfile />
+    </div>
+  )
 }
