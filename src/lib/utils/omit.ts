53ede34a92d373f436cac8822db680619e011d5c
bennyk0406 <kbenny0406@gmail.com>
2026-04-06 02:18:07 +0900

    delete omit.ts

--- src/lib/utils/omit.ts
+++
@@ -1,5 +0,0 @@
-export const omit = <T extends object, K extends keyof T>(obj: T, ...keys: Array<K>): Omit<T, K> =>
-    Object.fromEntries(
-        Object.entries(obj)
-            .filter(([key]) => !keys.includes(key as K))
-    ) as Omit<T, K>
